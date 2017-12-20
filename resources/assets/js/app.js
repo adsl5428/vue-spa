0db755074a5a42e36db89adcedaabe3b63de940a
lihongcheng <1519492056@qq.com>
2017-12-20 20:08:58 +0800

    lesson6

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,14 +1,22 @@
 
 require('./bootstrap');
+window.Vue = require('vue');
 
-// window.Vue = require('vue');
-import Vue from 'vue'
+
 import VueRouter from 'vue-router'
-
 import router from './routes'
 import App from './components/App'
 
+import zh_CN from './locale/zh_CN';
+import VeeValidate, { Validator } from 'vee-validate';
+Validator.localize('zh_CN', zh_CN);
+
 Vue.use(VueRouter)
+
+Vue.use(VeeValidate,{
+    locale:'zh_CN'
+})
+
 Vue.component('app',App)
 new Vue({
     el: '#app',
